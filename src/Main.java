662b52b229901e0fd07689bade2451f581905bfb
Eric Howard <ericjhowardjr@gmail.com>
2026-07-12 19:15:44 -0400

    Added player input

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        Character player = new Warrior("Eric");
-        Character enemy = new Goblin("Goby");
+        Character player;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--- WELCOME TO THE RING---");
+
+        System.out.print("Enter your hero's name: ");
+        String playerName = scanner.nextLine();
+
+        System.out.println("\nChoose your class:");
+        System.out.println("1. Warrior");
+        System.out.println("2. Mage");
+        System.out.print("Choice (1 or 2): ");
+        int classChoice = scanner.nextInt();
+
+        if (classChoice == 2) {
+            player = new Mage(playerName);
+        } else {
+            // Default to Warrior
+            player = new Warrior(playerName);
+        }
+
+        Character enemy = spawnRandomEnemy();
+
+        System.out.println("\nWatch out! A " + enemy.getName() + " appears!");
 
         Battle battle = new Battle(player, enemy);
-
         battle.startBattle();
+
+        scanner.close();
     }
+
+        private static Character spawnRandomEnemy() {
+            Random rand = new Random();
+            int roll = rand.nextInt(3);
+
+            if (roll == 0) {
+                return new Goblin("Sneaky Goby");
+            } else if (roll == 1) {
+                return new Goblin("Hobgoblin Bruiser");
+            } else {
+                return new Mage("Dark Cultist");
+            }
+        }
 
 }
 
